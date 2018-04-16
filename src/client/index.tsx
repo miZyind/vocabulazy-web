9a0b316613e8f8b7764ab99e10c4735715389367
miZyind <mizyind@gmail.com>
2018-04-16 18:10:35 +0800

    add tspaths & webpack alias feature

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,11 +4,11 @@ import { render } from 'react-dom';
 // Style
 import 'semantic-ui-css/semantic.min.css';
 // Container
-import App from './containers/app';
+import App from '@containers/app';
 // Env
 const name = process.env.APP_NAME!;
 const version = process.env.APP_VERSION!;
-import Landing from './components/landing-page';
+import Landing from '@components/landing-page';
 
 // render(<App name={name} version={version} />, document.getElementById('root'));
 render(<Landing />, document.getElementById('root'));
