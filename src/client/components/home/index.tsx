4f08d55d51fcd257849e0df4c62402113bc0d71a
miZyind <mizyind@gmail.com>
2018-04-17 03:10:32 +0800

    implement description component

--- src/client/components/home/index.tsx
+++ src/client/components/home/index.tsx
@@ -1,30 +1,19 @@
 // Node module
 import React from 'react';
-import { Container, Header, Segment, Button } from 'semantic-ui-react';
+import styled from 'styled-components';
+// Component
+import Description from './description';
 
 const Home = () => (
   <>
-    <Container text style={{ 'marginTop': '7em' }}>
-      <Header as='h1' textAlign='center' children='VocabuLazy' />
-      <Header as='h2' textAlign='center' style={{ 'fontFamily': 'PingFangTC-Medium' }}>
-        世界最好用的單字軟體
-        <Header.Subheader style={{ 'fontFamily': 'PingFangTC-Medium' }} children='讓你自由決定想背的單字' />
-      </Header>
-      <Segment textAlign='center' attached='top' compact style={{ 'border': 'none' }}>
-        <Button style={{ 'backgroundColor': '#9b9b9b', 'border': 'solid 1px #979797' }} children='開始使用' />
-      </Segment>
-      <Segment textAlign='right' attached='top' compact style={{ 'border': 'none' }}>
-        <Button icon='google' labelPosition='left' style={{ 'backgroundColor': '#9b9b9b', 'border': 'solid 1px #979797' }} content='Google Play' />
-        <Button icon='apple' labelPosition='left' style={{ 'backgroundColor': '#9b9b9b', 'border': 'solid 1px #979797' }} content='App Store' />
-      </Segment>
-    </Container>
-    <Container text>
+    <Description />
+    {/* <Container text>
       <Header as='h1' textAlign='center' children='全新研發背誦模式' />
       <Header as='h1' textAlign='center' children='讓你通勤也能專心學習' />
       <Header>
         <Header.Subheader children='讓你自由決定想背的東西' />
       </Header>
-    </Container>
+    </Container> */}
   </>
 );
 
