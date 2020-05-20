e25a13b7cfb59709d3dff9ab37340318eb4feb4a
miZyind <mizyind@gmail.com>
2020-05-20 11:22:13 +0700

    feat: add main pages

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Index = ({ className }: StyledProps) => (
+  <div className={className}>Web</div>
+);
+
+export default styled(Index)`
+  font-size: 50px;
+`;
